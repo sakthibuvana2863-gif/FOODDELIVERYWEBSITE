1ca078610b572a444bf8ba231a0b6155e4c9b4df
sakthibuvana2863@gmail.com <sakthibuvana2863@gmail.com>
2026-08-29 21:18:48 +0530

    Added user login with bcrypt authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,17 +18,48 @@ router.post("/register", async (req, res) => {
         });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    
+
     await db.collection("users").insertOne({
         name,
         email,
         password: hashedPassword
     });
 
-    
+
 
     res.json({
         message: "User Registered Successfully"
+    });
+});
+router.post("/login", async (req, res) => {
+
+    const db = req.app.locals.db;
+
+    const { email, password } = req.body;
+
+    const user = await db.collection("users").findOne({
+        email: email
+    });
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid Credentials"
+        });
+    }
+
+    const isMatch = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if (!isMatch) {
+        return res.status(400).json({
+            message: "Invalid Credentials"
+        });
+    }
+
+    res.json({
+        message: "Login Successful"
     });
 });
 
